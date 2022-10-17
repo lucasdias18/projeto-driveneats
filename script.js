3add8d0859e6bac1537cde34a540a3e815f51e98
Lucas da Silva Dias <lucasdias.ld10@gmail.com>
2022-10-17 16:00:24 -0300

    versão final

--- script.js
+++ script.js
@@ -6,10 +6,8 @@ function selecionarComida(botao) {
     botao.classList.add('selecionado')
     let elementos = document.querySelectorAll('.selecionado');
     if (elementos.length === 3) {
-        let button1 = document.querySelector('footer>button');
-        button1.classList.add('escondido')
-        let button2 = document.querySelector('.escondido');
-        button2.classList.remove('escondido')
+        let button = document.querySelector('footer');
+        button.innerHTML = `<button class="fechar-pedido" onclick="fecharPedido()">Fechar pedido</button>`
     }
 }
 
@@ -21,10 +19,8 @@ function selecionarBebida(botao) {
     botao.classList.add('selecionado')
     let elementos = document.querySelectorAll('.selecionado');
     if (elementos.length === 3) {
-        let button1 = document.querySelector('footer');
-        button1.firstChild.classList.add('escondido')
-        // let button2 = document.querySelector('.escondido');
-        button1.lastChild.classList.remove('escondido')
+        let button = document.querySelector('footer');
+        button.innerHTML = `<button class="fechar-pedido" onclick="fecharPedido()">Fechar pedido</button>`
     }
 }
 
@@ -36,9 +32,49 @@ function selecionarSobremesa(botao) {
     botao.classList.add('selecionado')
     let elementos = document.querySelectorAll('.selecionado');
     if (elementos.length === 3) {
-        let button1 = document.querySelector('footer>button');
-        button1.classList.add('escondido')
-        let button2 = document.querySelector('.escondido');
-        button2.classList.remove('escondido')
+        let button = document.querySelector('footer');
+        button.innerHTML = `<button class="fechar-pedido" onclick="fecharPedido()">Fechar pedido</button>`
     }
+}
+
+function fecharPedido() {
+
+    // PREÇO
+
+    const elemento = document.querySelectorAll('.selecionado>.preço>.valor');
+    // console.log(elemento)
+    let preçoTotal = 0;
+    for (let i=0; i<elemento.length; i++) {
+        let preçoUnidade = elemento[i].innerHTML
+        // console.log(preçoUnidade)
+        let x = Number(preçoUnidade.replace(',', '.'));
+        // console.log(typeof(x))
+        preçoTotal = preçoTotal + x;
+        // console.log(preçoTotal)
+    }
+
+    // COMIDA
+
+    const comida = document.querySelector('.comida>.selecionado>h2')
+    let nomeComida = comida.innerHTML
+
+    // BEBIDA
+
+    const bebida = document.querySelector('.bebida>.selecionado>h2')
+    let nomeBebida = bebida.innerHTML
+
+    // SOBREMESA
+
+    const sobremesa = document.querySelector('.sobremesa>.selecionado>h2')
+    let nomeSobremesa = sobremesa.innerHTML
+
+    // MENSAGEM WPP
+
+    let pedidoFinal = encodeURIComponent(`Olá, gostaria de fazer o pedido:
+    - Prato: ${nomeComida}
+    - Bebida: ${nomeBebida}
+    - Sobremesa: ${nomeSobremesa}
+    Total: R$ ${preçoTotal.toFixed(2).replace('.', ',')}`)
+
+    window.open(`https://wa.me/5535992049912?text=` + pedidoFinal)
 }
